a97491b7294b6faf323058cee635684777ee3e3e
Utopiality <deepl.lead0907@gmail.com>
2024-07-22 19:51:33 -0400

    fix: uncomment

--- uxops/src/lib/supabase/middleware.ts
+++ uxops/src/lib/supabase/middleware.ts
@@ -30,68 +30,68 @@ export const updateSession = async (request: NextRequest) => {
 
     const { pathname } = request.nextUrl;
 
-    // const supabase = createServerClient(
-    //   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    //   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    //   {
-    //     cookies: {
-    //       get(name: string) {
-    //         return request.cookies.get(name)?.value;
-    //       },
-    //       set(name: string, value: string, options: CookieOptions) {
-    //         // If the cookie is updated, update the cookies for the request and response
-    //         request.cookies.set({
-    //           name,
-    //           value,
-    //           ...options,
-    //         });
-    //         response = NextResponse.next({
-    //           request: {
-    //             headers: request.headers,
-    //           },
-    //         });
-    //         response.cookies.set({
-    //           name,
-    //           value,
-    //           ...options,
-    //         });
-    //       },
-    //       remove(name: string, options: CookieOptions) {
-    //         // If the cookie is removed, update the cookies for the request and response
-    //         request.cookies.set({
-    //           name,
-    //           value: '',
-    //           ...options,
-    //         });
-    //         response = NextResponse.next({
-    //           request: {
-    //             headers: request.headers,
-    //           },
-    //         });
-    //         response.cookies.set({
-    //           name,
-    //           value: '',
-    //           ...options,
-    //         });
-    //       },
-    //     },
-    //   }
-    // );
+    const supabase = createServerClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      {
+        cookies: {
+          get(name: string) {
+            return request.cookies.get(name)?.value;
+          },
+          set(name: string, value: string, options: CookieOptions) {
+            // If the cookie is updated, update the cookies for the request and response
+            request.cookies.set({
+              name,
+              value,
+              ...options,
+            });
+            response = NextResponse.next({
+              request: {
+                headers: request.headers,
+              },
+            });
+            response.cookies.set({
+              name,
+              value,
+              ...options,
+            });
+          },
+          remove(name: string, options: CookieOptions) {
+            // If the cookie is removed, update the cookies for the request and response
+            request.cookies.set({
+              name,
+              value: '',
+              ...options,
+            });
+            response = NextResponse.next({
+              request: {
+                headers: request.headers,
+              },
+            });
+            response.cookies.set({
+              name,
+              value: '',
+              ...options,
+            });
+          },
+        },
+      }
+    );
 
     // This will refresh session if expired - required for Server Components
     // https://supabase.com/docs/guides/auth/server-side/nextjs
-    // const {
-    //   data: { user },
-    // } = await supabase.auth.getUser();
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
 
-    // const isAuthenticated = !!user;
-    // console.log('*********', user);
+    const isAuthenticated = !!user;
+    console.log('*********', user);
 
-    // if (!isAuthenticated && isInAuthRoute(pathname)) {
-    //   const url = request.nextUrl.clone();
-    //   url.pathname = '/signin';
-    //   return NextResponse.redirect(url);
-    // }
+    if (!isAuthenticated && isInAuthRoute(pathname)) {
+      const url = request.nextUrl.clone();
+      url.pathname = '/signin';
+      return NextResponse.redirect(url);
+    }
 
     return response;
   } catch (e) {
