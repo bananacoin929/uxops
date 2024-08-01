d5d1c2ffc5873ffb5ccff9facfa2a50e3a8970e6
Utopiality <deepl.lead0907@gmail.com>
2024-08-01 03:52:08 -0400

    fix: next config

--- uxops/next.config.mjs
+++ uxops/next.config.mjs
@@ -2,6 +2,11 @@ import './src/env.mjs';
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  env: {
+    SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY,
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
   images: {
     remotePatterns: [
       {
